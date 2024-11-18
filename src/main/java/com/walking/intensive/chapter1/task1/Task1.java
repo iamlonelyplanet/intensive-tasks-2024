a2418dc31069abb0b6a6cb7db9c3677e6a04e731
iamLonelyPlanet <aleksey.nizamov@gmail.com>
2024-11-18 09:23:24 +0500

    Готово

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,14 +15,24 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        byte age = 127;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        if (age < 0) {
+            return "Некорректный ввод";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        if ((age >= 11 && age <= 14) || (age >= 111 && age <= 114) || (age % 10 >= 5 || age % 10 == 0)) {
+            return "Вам " + age + " лет";
+        }
+        if (age % 10 == 1) {
+            return "Вам " + age + " год";
+        }
+
+
+        return "Вам " + age + " года";
     }
 }
