ed6093de52ea018da215a404403a6bfda68b7131
iamLonelyPlanet <aleksey.nizamov@gmail.com>
2024-11-21 17:57:23 +0500

    Первая версия. Долбанные радианы.

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter1.task5;
 
+import java.util.Arrays;
+
 /**
  * Задача поиска площади, величин углов, длин высот, биссектрис, медиан, радиусов вписанной и описанной вокруг
  * треугольника окружностей является центральной в Геометрии.
@@ -10,7 +12,18 @@ package com.walking.intensive.chapter1.task5;
  */
 public class Task5 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        double a = 3;
+        double b = 4;
+        double c = 5;
+
+        System.out.println("Площадь по Герону: " + getAreaByHeron(a, b, c));
+        System.out.println("Высоты: " + Arrays.toString(getHeights(a, b, c)));
+        System.out.println("Медианы: " + Arrays.toString(getMedians(a, b, c)));
+        System.out.println("Биссектрисы: " + Arrays.toString(getBisectors(a, b, c)));
+        System.out.println("Углы: " + Arrays.toString(getAngles(a, b, c)));
+        System.out.println("Радиус вписанной окружности: " + getInscribedCircleRadius(a, b, c));
+        System.out.println("Радиус описанной окружности: " + getCircumradius(a, b, c));
+        System.out.println("Площадь по-продвинутому: " + getAreaAdvanced(a, b, c));
     }
 
     /**
@@ -22,10 +35,20 @@ public class Task5 {
      *
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
-    static double getAreaByHeron(double a, double b, double c) {
-        //        Место для вашего кода
+    static boolean isTriangleImpossible(double a, double b, double c) {
+        return a <= 0 || b <= 0 || c <= 0 || a + b < c || a + c < b || c + b < a;
+    }
 
-        return 0; // Заглушка. При реализации - удалить
+    static double getHalfPerimeter(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    static double getAreaByHeron(double a, double b, double c) {
+        if (isTriangleImpossible(a, b, c)) {
+            return -1;
+        }
+        double p = getHalfPerimeter(a, b, c);
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     /**
@@ -36,9 +59,19 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getHeights(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (isTriangleImpossible(a, b, c)) {
+            return new double[0];
+        }
+        double[] heights = new double[3];
+        double twoAreas = 2 * getAreaByHeron(a, b, c);
+        heights[0] = twoAreas / a;
+        heights[1] = twoAreas / b;
+        heights[2] = twoAreas / c;
+        System.out.println(heights[0]);
+        System.out.println(heights[1]);
+        System.out.println(heights[2]);
+        Arrays.sort(heights);
+        return heights;
     }
 
     /**
@@ -49,9 +82,15 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getMedians(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (isTriangleImpossible(a, b, c)) {
+            return new double[0];
+        }
+        double[] medians = new double[3];
+        medians[0] = 0.5 * Math.sqrt(2 * Math.pow(a, 2) + 2 * Math.pow(b, 2) - Math.pow(c, 2));
+        medians[1] = 0.5 * Math.sqrt(2 * Math.pow(b, 2) + 2 * Math.pow(c, 2) - Math.pow(a, 2));
+        medians[2] = 0.5 * Math.sqrt(2 * Math.pow(c, 2) + 2 * Math.pow(a, 2) - Math.pow(b, 2));
+        Arrays.sort(medians);
+        return medians;
     }
 
     /**
@@ -62,9 +101,16 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getBisectors(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (isTriangleImpossible(a, b, c)) {
+            return new double[0];
+        }
+        double[] bisectors = new double[3];
+        double perimeter = 2 * getHalfPerimeter(a, b, c);
+        bisectors[0] = (Math.sqrt(a * b * perimeter * (a + b - c))) / (a + b);
+        bisectors[1] = (Math.sqrt(b * c * perimeter * (b + c - a))) / (b + c);
+        bisectors[2] = (Math.sqrt(c * a * perimeter * (c + a - b))) / (c + a);
+        Arrays.sort(bisectors);
+        return bisectors;
     }
 
     /**
@@ -75,9 +121,15 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getAngles(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (isTriangleImpossible(a, b, c)) {
+            return new double[0];
+        }
+        double[] angles = new double[3];
+        angles[0] = Math.toDegrees(Math.acos((Math.pow (a, 2) + Math.pow (c, 2) - Math.pow (b, 2)) / (2 * a * c)));
+        angles[1] = Math.toDegrees(Math.acos((Math.pow (b, 2) + Math.pow (a, 2) - Math.pow (c, 2)) / (2 * b * a)));
+        angles[2] = Math.toDegrees(Math.acos((Math.pow (c, 2) + Math.pow (b, 2) - Math.pow (a, 2)) / (2 * c * b)));
+        Arrays.sort(angles);
+        return angles;
     }
 
     /**
@@ -88,9 +140,10 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getInscribedCircleRadius(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        if (isTriangleImpossible(a, b, c)) {
+            return -1;
+        }
+        return getAreaByHeron(a, b, c) / getHalfPerimeter(a, b, c);
     }
 
     /**
@@ -101,9 +154,10 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getCircumradius(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        if (isTriangleImpossible(a, b, c)) {
+            return -1;
+        }
+        return a * b * c / (4 * getAreaByHeron(a, b, c));
     }
 
     /**
@@ -121,8 +175,12 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getAreaAdvanced(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        if (isTriangleImpossible(a, b, c)) {
+            return -1;
+        }
+        double cosinusA = (Math.pow(b, 2) + Math.pow(c, 2) - Math.pow(a, 2)) / (2 * b * c);
+        double sinusA = Math.sqrt(1-Math.pow(cosinusA, 2)); // в радианах!
+        // S = 1/2ab sin(c) - площадь через 2 стороны и угол
+        return b * c * sinusA / 2;
     }
 }
