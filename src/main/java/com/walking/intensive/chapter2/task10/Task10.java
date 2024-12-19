6f7a6a0cf470905053c5fb614ebfaf9bacf07d2b
iamLonelyPlanet <aleksey.nizamov@gmail.com>
2024-12-19 17:35:37 +0500

    Task 10. Второй метод палиндрома - чисто для себя.

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,7 +1,5 @@
 package com.walking.intensive.chapter2.task10;
 
-import java.util.Locale;
-
 /**
  * Реализуйте метод isPalindrome(), который проверяет, является ли строка палиндромом.
  *
@@ -15,64 +13,54 @@ public class Task10 {
     public static void main(String[] args) {
         String inputString1 = "   Муза,   ранясь !!! )шилом (опыта), ты помолишься на разум!?!   ";
         String inputString2 = "Ежу хуже";
-        String inputString = "гааг ";
+        String inputString = "!!!!гаа12г";
         System.out.println(isPalindrome(inputString));
         //символы нижней кириллицы: [1072,1103] + 1105
     }
 
     static boolean isPalindrome(String inputString) {
-        if (inputString == null || inputString.length() <= 1 || inputString.length() == 0) {
+        if (inputString == null || inputString.length() <= 1 || inputString.isEmpty()) {
             return false;
         }
 
         inputString = inputString.toLowerCase();
-        int halfLength = inputString.length() / 2;
-        int i = 0;
-        char leftLetter = inputString.charAt(i);
-        while (i <= halfLength) {
-            leftLetter = inputString.charAt(i);
-            if (isCyrillicLetter(leftLetter)) {
-                leftLetter = inputString.charAt(i);
-                char rightLetter = getRightLetter(inputString, i);
-                System.out.println("Сравниваем букву " + leftLetter + " с буквой " + rightLetter);
-                if (leftLetter != rightLetter) {
-                    System.out.println("Они не одинаковы!");
-                    return false;
-                }
-            }
-            i++;
-        }
-        return leftLetter == getRightLetter(inputString, i - 1);
-    }
 
-    static char getRightLetter(String inputString, int a) {
-        int j = inputString.length() - a - 1;
-        int halfLength = inputString.length() / 2;
-        int odd = inputString.length() % 2 == 0 // Чётность приведённой строки. Чтобы проверить лишь до половины строки.
-                ? 1
-                : 0;
-        char rightLetter = inputString.charAt(halfLength * 2 - a - odd);
-        while (j > halfLength) {
-            rightLetter = inputString.charAt(j);
-            if (isCyrillicLetter(rightLetter)) {
-                rightLetter = inputString.charAt(j);
-                break;
+        int leftSymbol = 0;
+        int rightSymbol = inputString.length() - 1;
+
+        while (leftSymbol < rightSymbol) {
+            while (leftSymbol < rightSymbol && !isCyrillicLetter((inputString.charAt(leftSymbol)))) {
+                leftSymbol++;
             }
-            j--;
+
+            while (leftSymbol < rightSymbol && !isCyrillicLetter((inputString.charAt(rightSymbol)))) {
+                rightSymbol--;
+            }
+
+            if (inputString.charAt(leftSymbol) != inputString.charAt(rightSymbol)) {
+                return false;
+            }
+
+            leftSymbol++;
+            rightSymbol--;
         }
-        return rightLetter;
+
+        return true;
     }
 
     static boolean isCyrillicLetter(char letter) {
-        return ((int) letter >= 1072 && (int) letter <= 1105 && (int) letter != 1104);
+        return (int) letter >= 1072 && (int) letter <= 1105 && (int) letter != 1104;
     }
 
     static boolean isPalindromeByExceptions(String inputString) {
+        /* Метод работает по принципу удаления пробелов и знаков препинания. Недостаток метода - низкая (вроде!)
+        скорость; невозможность удалить цифры, латинские буквы, смайлики и это вот всё.
+         */
+
         if (inputString == null) {
             return false;
         }
 
-        // Удаляем пробелы и знаки препинания, затем переводим всё в нижний регистр
         inputString = inputString.replace(" ", "");
         inputString = inputString.replace(".", "");
         inputString = inputString.replace("!", "");
